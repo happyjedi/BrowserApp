c14ed39b152ccf00686a0b4f1b17eef7a8216958
happyjedi <exploit.vn@yandex.ru>
2016-01-05 17:58:23 +0300

    Added Browser logic

--- BrowserApp/ViewController.swift
+++ BrowserApp/ViewController.swift
@@ -9,17 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var WebView: UIWebView!
+    @IBOutlet weak var ActInd: UIActivityIndicatorView!
+    @IBOutlet weak var SearchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        SearchBar.text = "http://"
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    func searchBarSearchButtonClicked(searchbar: UISearchBar) {
+        
+        searchbar.resignFirstResponder()
+        
+        let text = SearchBar.text
+        let url = NSURL(string: text!)
+        let request = NSURLRequest(URL: url!)
+        
+        WebView.loadRequest(request)
+        
+    }
+    
+    func webViewDidStartLoad(_ : UIWebView) {
+        
+        ActInd.startAnimating()
+        
+    }
+    
+    func webViewDidFinishLoad(_ : UIWebView) {
+        
+        ActInd.stopAnimating()
+        
+    }
 
 }
 
